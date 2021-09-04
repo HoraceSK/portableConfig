aabd967588f788f488289b785273dca8781ba913
zefuirusu <zefuirusu@qq.com>
2021-09-04 21:07:21 +0800

    vim and bashrc conofig update

--- configFiles/bashrc-config-2021-0904.sh
+++ configFiles/bashrc-config-2021-0904.sh
@@ -8,7 +8,10 @@ PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\][\u]\[\033[00m\][\[\033[01
 =======
 #PS1="=>[\#][\u][\w]\$ "
 #PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\][\u]\[\033[00m\][\[\033[01;34m\]\w\[\033[00m\]]\n==>[\#]\$ '
+
+# ok:
 PS1='\[\033[01;32m\][\u]\[\033[00m\][\[\033[01;34m\]\w\[\033[00m\]]\n==>[\#]\$ '
+
 >>>>>>> 91a63bcc68b1dc6a93df98b978e04b2c340b5872:config/bashrc-config-2020-1107.txt
 
 ## 命令简化：
@@ -16,10 +19,12 @@ alias rm='rm -i'
 alias ll="ls -hl --color=auto"
 alias cl='clear'
 alias cll='clear & ls -hl'
+alias how='du -h -d 1 ./'
 
 alias sshgo="systemctl start sshd.service"
 alias sshoff="systemctl stop sshd.service"
 
+# 挂载坚果云网盘，注意需要一个临时密码，需要登录网页版坚果云设置获取
 alias mdav='sudo mount -t davfs https://dav.jianguoyun.com/dav/ ~/media/y-nutStore/'
 
 ## 启动软件：
@@ -37,6 +42,10 @@ alias vlc="/usr/bin/vlc"
 alias ffx="/opt/firefox/firefox"
 alias ffd="/opt/firefox/firefox & exit"
 alias tmv="/opt/teamviewer/teamviewer & exit"
+
+# virtual machine manage:
+alias debianon='vboxmanage startvm debian --type headless'
+alias debianoff='vboxmanage controlvm debian poweroff'
 
 # functions:
 function findf (){
